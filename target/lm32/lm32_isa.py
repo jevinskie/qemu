93dde3b69a892b984424edd725a7a5aaa8648d8e
Jevin Sweval <jevinsweval@gmail.com>
2025-06-29 16:17:13 -0400

    wip

--- target/lm32/lm32_isa.py
+++ target/lm32/lm32_isa.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
+from collections import namedtuple
 from enum import Enum, auto
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Final
 
 import pprint
 
 
 OpBits: Final[int] = 6
+
 
 class Format(Enum):
     RI = auto()
@@ -22,21 +24,23 @@ class Format(Enum):
 
 @dataclass
 class LM32Instr:
-    mnemonic: str
+    mnemonic: str = field(init=False)
     opc: int
     fmt: Format
 
+    def __set_name__(self, owner: type, name: str) -> None:
+        self.mnemonic = name
 
+
+@dataclass
 class LM32Instrs:
-    srui: Final = LM32Instr("srui", 0b000_000, Format.RI5)
-    nori: Final = LM32Instr("nori", 0b000_001, Format.RI)
-    # FIXME: need the following with either a descriptor or metaclass - I forget
-    # srui: Final = LM32Instr(0b000_000, Format.RI5)
-    # nori: Final = LM32Instr(0b000_001, Format.RI)
+    srui: Final = LM32Instr(0b000_000, Format.RI5)
+    nori: Final = LM32Instr(0b000_001, Format.RI)
+
 
 LM32_INSTRS: tuple[LM32Instr, ...] = (
     LM32Instrs.srui,
-    LM32Instr("nori", 0b000_001, Format.RI),
+    LM32Instrs.nori,
 )
 
 
